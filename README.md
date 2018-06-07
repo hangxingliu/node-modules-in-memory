93d1ecdbf8962d34c0ff995e450ee345f30a3d35
Liu Yue <hangxingliu@gmail.com>
2018-06-07 14:44:45 +0800

    add initialize script and documents

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Put your `node_modules` into your memory for Linux (maybe MacOS) user has enough memory.
 
+## Install
+
+``` bash
+git clone https://github.com/hangxingliu/node-modules-in-memory
+# for example: your current directory is /path/to/
+# then append a command into your "~/.bashrc" or "~/.bash_profile" (or equivalent)
+echo 'source /path/to/node-modules-in-memory/init.sh;' >> ~/.bashrc;
+```
+
 ## Usage
 
 ``` bash
--- init.sh
+++ init.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+# Usage:
+#
+#  append following command in the one of "~/.bashrc", "~/.zshrc" or "~/.bash_profile" ...:
+#
+#    source /path/to/node-modules-in-memory/init.sh
+#
+
 _nmim_completion() {
 	local ACTIONS OPTS_COMMON OPTS_START;
-	ACTIONS="start stop save";
-
-	OPTS_COMMON="";
-	OPTS_START="${OPTS_COMMON} --size";
+	ACTIONS="start stop save stop-all";
+	OPTS_START="--size";
 
 	local PREV_WORD CURRENT_WORD;
 	CURRENT_WORD="${COMP_WORDS[COMP_CWORD]}";
     PREV_WORD="${COMP_WORDS[COMP_CWORD-1]}";
 
+	# actions completion
 	if [[ "$COMP_CWORD" == "1" ]]; then
         COMPREPLY=( $( compgen -W "$ACTIONS" -- $CURRENT_WORD ) );
 		return;
@@ -20,7 +26,6 @@ _nmim_completion() {
 	CURRENT_ACTION="${COMP_WORDS[1]}";
 	case "$CURRENT_ACTION" in
 		start)
-			# completion for list
 			if [[ "$CURRENT_WORD" == -* ]]; then
 				COMPREPLY=( $( compgen -W "$OPTS_START" -- $CURRENT_WORD ) );
 				return;
@@ -28,8 +33,6 @@ _nmim_completion() {
 			# case "$PREV_WORD" in
 			# 	"-F") ;& "--format")
 			# 		COMPREPLY=( $( compgen -W "normal reverse" -- $CURRENT_WORD ) );;
-			# 	"--style")
-			# 		COMPREPLY=( $( compgen -W "${AVAILABLE_STYLE}" -- $CURRENT_WORD ) );;
 			# 	*) COMPREPLY=( $( compgen -A file -- $CURRENT_WORD ) );;
 			# esac
 		;;
@@ -39,5 +42,23 @@ _nmim_completion() {
 	esac
 }
 
-complete -F _nmim_completion 'nmim';
+function __init_nmim() {
+	local __DIRNAME BIN_DIR BIN_FILE WHICH_BIN;
+	__DIRNAME=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`;
+	BIN_DIR="$__DIRNAME/bin";
+	BIN_FILE="nmim";
 
+	WHICH_BIN=`which $BIN_FILE`;
+	if [[ -z "$_TEST" ]]; then
+		export PATH="$PATH:$BIN_DIR";
+	elif [[ "$WHICH_BIN" != "$BIN_DIR/$BIN_FILE" ]]; then
+		echo -e "error: init node-modules-in-memory failed!" > /dev/stderr;
+		echo -e "         there has a existed nmim in this system: $WHICH_BIN" > /dev/stderr;
+		return 1;
+	fi
+
+	# register completion function
+	complete -F _nmim_completion "$BIN_FILE";
+}
+
+__init_nmim;
